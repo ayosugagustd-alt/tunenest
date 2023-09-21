3a6092cf937072cacf472a1c848e8d08cf76a986
hiro <jmusic.hiro@gmail.com>
2023-09-21 09:40:48 +0900

    import宣言の仕方

--- usviral50.py
+++ usviral50.py
@@ -1,5 +1,9 @@
 # 必要なライブラリをインポート
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 import os
 import unicodedata
