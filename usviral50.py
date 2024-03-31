c0edd2320a6099a12d3e28d13c26e6f842d944e5
hiro <jmusic.hiro@gmail.com>
2024-03-31 11:43:07 +0900

    トッププレイリスト更新

--- usviral50.py
+++ usviral50.py
@@ -116,8 +116,8 @@ def static_from_root():
 @app.route("/")
 def index():
     try:
-        # デフォルトID(Tokyo Super Hits)かクエリパラメータIDを設定
-        playlist_id = request.args.get("playlist_id", "37i9dQZF1DXebjPzM8sHrq")
+        # デフォルトIDかクエリパラメータIDを設定
+        playlist_id = request.args.get("playlist_id", "37i9dQZF1DWUQlpHXZRO4T")
 
         # Spotifyクライアントを取得
         sp = get_spotify_client()
