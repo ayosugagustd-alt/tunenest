e560bdf3526a6aef4b855e341446c3936f27025f
hiro <jmusic.hiro@gmail.com>
2024-05-31 09:11:19 +0900

    Pop to New

--- usviral50.py
+++ usviral50.py
@@ -46,7 +46,7 @@ try:
         # playlistsはインデックスページのルーティング処理で参照する
         playlists = json.load(f)
         default_playlist_id = next(
-            iter(playlists.get("Pop", {}).keys())
+            iter(playlists.get("New", {}).keys())
         )  # 最初のキーをデフォルトIDとして使用
 except FileNotFoundError:
     logging.warning("playlists.jsonが見つかりません。")
